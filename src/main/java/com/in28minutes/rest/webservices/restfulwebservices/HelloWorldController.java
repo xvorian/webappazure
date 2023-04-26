52aa8f59b8cc50adfe66d66890544191a6b4e482
Amjad Hanif <amjad.hanif@hotmail.com>
2023-04-26 01:36:09 -0400

    testing staging slot

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -16,7 +16,7 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldBean() {
 		// throw new RuntimeException("Some Error has Happened! Contact Support at
 		// ***-***");
-		return new HelloWorldBean("Hello World");
+		return new HelloWorldBean("Hello World on Staging");
 	}
 
 	/// hello-world/path-variable/in28minutes
